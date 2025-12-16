fc1c9ac1987d421e68c6649e62d178b866ede402
unknown <pc@DESKTOP-9570JPM.ictl2.com>
2025-12-16 23:55:19 +0100

    Ajout du fichier principe solide SRP

---
+++ SRP/Book.java
@@ -0,0 +1,37 @@
+public class Book{
+    private String title;
+    private String author;
+    private String content;
+
+    public Book(String title, String author, String content){
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getTitle(){return title;}
+    public String getAuthor(){return author;}
+    public String getContent(){return content;}
+
+    public void printToScreen(){
+        System.out.println("Titre:" + title);
+        System.out.println("Auteur:" + author);
+        System.out.println("Contenu:" + content);
+    }
+
+    public void saveToDatabase(){
+        System.out.println("Sauvegarde du livre"+ title +"en base de donnee");
+    }
+    public void emprunter(String lecteur){
+        System.err.println("Emprunt du livre"+ title +" par" +lecteur);
+    }
+
+   
+        public static void main(String[] args) {
+            Book book = new Book("Les principes SOLID", "Etudiants de M1-GL","Revision des principes SOLID");
+            book.printToScreen();
+            book.saveToDatabase();
+            book.emprunter("Marcial");
+        }
+
+    }
